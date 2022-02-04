9b5d48195d6e0f854c536ada3a866cc38c795bee
Alessandro Mandaglio <ale.manda.30@gmail.com>
2022-02-04 17:04:11 +0100

    Sass

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <title>Movie</title>
 </head>
 <body>
   <div class="container">
@@ -19,7 +20,7 @@
                 <p>{{$item['date']}}</p>
             </li>
             <li>
-                <h6>{{$item['vote']}}</h6>
+                <h5>{{$item['vote']}}</h5>
             </li>
         </ul>
     </div>
